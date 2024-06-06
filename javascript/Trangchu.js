f1418b13ff106189d2b7425ccd58162ef0941159
huzzg <huzzxje@gmail.com>
2024-06-07 02:04:54 +0700

    Update

---
+++ javascript/Trangchu.js
@@ -0,0 +1,16 @@
+function dangBai() {
+    var userInput = document.getElementById("post-input").value;
+    console.log("Đã đăng bài:", userInput);
+  }
+  
+  function themIcon() {
+    alert("Không có icon");
+  }
+  
+  function dangAnhVideo() {
+    console.log("Đã đăng ảnh/video");
+  }
+
+  function thichPost() {
+    alert("Bạn đã thích nội dung này");
+  }
